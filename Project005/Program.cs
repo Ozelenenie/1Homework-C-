bafeb6438e067766ee145755d94f10ea91636661
Kate <kd32107@inbox.ru>
2022-12-18 17:48:13 +0500

    add project 005 condition if-else

---
+++ Project005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "катя")
+{
+    Console.WriteLine("Ура!!! Катюха!!!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
